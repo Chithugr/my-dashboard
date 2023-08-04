2a71b4289beebae3e229e879ad373930720b07a1
chaithra <113985489+chaithragr06@users.noreply.github.com>
2023-08-04 17:15:38 +0530

    list Changes commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,16 +3,23 @@ import styles from './Sidebar.module.css'; // Import the CSS module
 
 function Sidebar() {
     return (
+
         <div className={styles.sidebar}>
+            <h2>Dashboard</h2>
             <nav>
                 <ul>
                     <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>E-Mail</li>
-                    <li>Chat</li>
-                    <li>Calender</li>
+                    <li>Start_Year</li>
+                    <li>Title</li>
+                    <li>Published</li>
+                    <li>Source</li>
+                    <li>Title</li>
+                    <li>Country</li>
+                    <li>Region</li>
+                    <li>City</li>
                     <li>Invoice</li>
+                    <li>End_Year</li>
+
                     {/* Add more navigation links as needed */}
                 </ul>
             </nav>
